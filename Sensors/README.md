42721b7fd97b4afbdb4f8e2f8b916dcb0696f454
dtrilsbeek <d.trilsbeek@gmail.com>
2019-11-28 14:15:23 +0100

    Working push to api

--- Sensors/README.md
+++ Sensors/README.md
@@ -1,2 +1,4 @@
 Resources used
 http://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-the-raspberry-pi/
+
+https://learn.adafruit.com/adafruit-am2320-temperature-humidity-i2c-sensor/python-circuitpython 
--- Sensors/humidtemp.py
+++ Sensors/humidtemp.py
@@ -2,19 +2,17 @@
 import json
 import time
 import requests
+import board
+import busio
+import adafruit_am2320
 
-
-# import sys
-# import Adafruit_DHT
-# while True:
-#
-#     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-#
-#     print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
+# create the I2C shared bus
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_am2320.AM2320(i2c)
 
 
 def posthumidity(value):
-    print("Posting to API")
+    print("Posting humidity to API")
 
     url = 'http://192.168.43.4:8090/sensors/'
     payload = {
@@ -26,7 +24,25 @@ def posthumidity(value):
     print(response.text)
 
 
+def posttemp(value):
+    print("Posting temp to API")
+
+    url = 'http://192.168.43.4:8090/sensors/'
+    payload = {
+        'type': 'temp',
+        'value': value
+    }
+    headers = {'content-type': 'application/json'}
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    print(response.text)
+
+
 starttime = time.time()
 while True:
-    posthumidity(99)
+    print("Temperature: ", sensor.temperature)
+    print("Humidity: ", sensor.relative_humidity)
+
+    posthumidity(sensor.relative_humidity)
+    posttemp(sensor.temperature)
+
     time.sleep(5.0 - ((time.time() - starttime) % 5.0))
